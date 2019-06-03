e81e6eacb0b8781954be20fb09beda4ac5595001
arosch <a.roschlaub@tum.de>
2019-06-03 16:44:00 +0200

    refactor of algorithm and code for stack abstraction

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ dir="${0%/*}"
 (cd $dir/build && make)
 
 # execution
-$dir/build/evmBcAnalyzer $1 $dir/output/graph.gv $dir/output/graph2.gv $dir/output/graph3.gv
+$dir/build/evmBcAnalyzer $1 $2 $dir/output/graph.gv $dir/output/graph2.gv $dir/output/graph3.gv
 
 # generate the ps cfgs
 dot -Tps $dir/output/graph.gv -o $dir/output/graph_creation.ps
